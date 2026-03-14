e3b417469d351e94893dfec3321109d27bad1668
Carlos Ledoux <carlosledoux@gmail.com>
2026-03-15 08:15:24 +0900

    Validation on Items and Want to Buy

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :house
+
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :price, numericality: { greater_than: 0 }
 end
--- app/models/want_to_buy.rb
+++ app/models/want_to_buy.rb
@@ -1,4 +1,6 @@
 class WantToBuy < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  validates :notes, length: { maximum: 1000 }, allow_blank: true
 end
