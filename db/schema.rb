7569395cb9bb0461ffac1ed5533e497c4df193ef
Carlos Ledoux <carlosledoux@gmail.com>
2026-03-14 22:17:56 +0900

    First Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_14_122141) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_14_131350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.date "end_date", null: false
+    t.string "name"
+    t.string "owner", null: false
+    t.date "start_date", null: false
+    t.string "tags", default: [], array: true
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "age"
+    t.string "category"
+    t.string "condition"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.bigint "house_id", null: false
+    t.string "name"
+    t.decimal "price", precision: 10, scale: 2
+    t.string "tags", default: [], array: true
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_items_on_category"
+    t.index ["house_id"], name: "index_items_on_house_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,8 +48,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_14_122141) do
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "role", default: "customer", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "want_to_buys", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "item_id", null: false
+    t.text "notes"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["item_id"], name: "index_want_to_buys_on_item_id"
+    t.index ["user_id"], name: "index_want_to_buys_on_user_id"
+  end
+
+  add_foreign_key "items", "houses"
+  add_foreign_key "want_to_buys", "items"
+  add_foreign_key "want_to_buys", "users"
 end
