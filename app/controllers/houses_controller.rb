4f07eb44eb637a1a637797efa76083345747f963
carlos ledoux <carlosledoux@gmail.com>
2026-04-15 17:20:09 +0900

    Opening Past Sales to not require log in

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,5 @@
 class HousesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :old_sales]
 
   def index
     @houses = House.where("end_date > ?", Date.today).where(deleted_at: nil)
